836de577f202c8082ef94456c475051f5e974e0b
twwwwwwwwww <vvvvv9766@gmail.com>
2025-06-04 14:39:24 +0900

    20250604 두번째 업데이트

--- src/20250602.js
+++ src/20250602.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route,Routes,Link } from 'react-router-dom'
+import { Route,Routes } from 'react-router-dom'
 import Home from './component/Home'
 import About from './component/About'
 import Head from './component/Head'
